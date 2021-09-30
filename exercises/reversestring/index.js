0b031778b7aee5169d4258cf868986165f92f6e0
unknown <cmanigos@gmail.com>
2021-09-30 14:29:25 +0800

    Start Palindromes

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,44 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    debugger;
+    return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+reverse('apple');
 
 module.exports = reverse;
+
+// Solution #1
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// Solution #2
+// function reverse(str) {
+//     let reversed = '';
+  
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+  
+//     return reversed;
+// }
+
+// Solution #3
+// function reverse(str) {
+//     return str.split('').reduce((rev, char) => char + rev, '');
+// }
+
+// How to Debug
+// function reverse(str) {
+//     debugger;
+//     return str.split('').reduce((rev, char) => char + rev, '');
+// }
+
+// reverse('apple');
